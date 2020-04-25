b1415c5f93db8156e7b07ad2ac62bdb6eec44f0a
Serena Sensini <s.sensini@gruppoisc.com>
2020-04-25 17:36:08 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import {LaunchNavigator} from '@ionic-native/launch-navigator/ngx';
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,7 +17,8 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+      LaunchNavigator
   ],
   bootstrap: [AppComponent]
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {LaunchNavigator} from '@ionic-native/launch-navigator/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,37 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  restaurants = [
+    {
+      lat: 12.478737,
+      lon: 41.847545,
+      address: 'Via Laurentina, 15, 00142 Roma RM',
+      name: 'Zumi Sushi Restaurant'
+    },
+    {
+      lat: 12.4366479,
+      lon: 41.9713835,
+      address: 'Via di Grottarossa, 52, 00189 Roma RM',
+      name: 'Kabab Iranian Restaurant'
+    },
+    {
+      lat: 12.4595611,
+      lon: 41.8287609,
+      address: 'Viale Oceania, 90, 00144 Roma RM',
+      name: 'Giolitti Eur'
+    }
+  ];
 
+  constructor(private launchNavigator: LaunchNavigator) {}
+
+  goTo(res) {
+    const lat = res.lat;
+    const lon = res.lon;
+    this.launchNavigator
+        .navigate([lon, lat], { start: 'Milano, IT'})
+        .then(
+            success => console.log('Avviato navigatore!'),
+            error => console.log('Errore: ', error)
+        );
+  }
 }
